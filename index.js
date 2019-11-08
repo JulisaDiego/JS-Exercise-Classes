e4a87087f254bb7e4f3efa9cfacc7218d5772fd3
JulisaDiego <julisadiego@yahoo.com>
2019-11-07 22:22:06 -0500

    added to JS-Classes

--- index.js
+++ index.js
@@ -175,27 +175,15 @@ class Instructor extends Lambdasian {
   this.catchPhrase = attributes.catchPhrase;
 }
 
-
-
+demo(){
+  return `Today we are learning about ${subject}`
 }
 
-const instructorAttr = {
-  name: 'Luis',
-  age: 45,
-  location: 'Provo',
-  specialty: 'SQL',
-  favLanguage: 'C#',
-  catchPhrase: 'Don\'t forget the homies'
+grade() {
+  return `${student.name} receives a perfect score on ${subject}`
+}
 }
 
-const studentAttr = {
-  name: 'Matt',
-  age: 30,
-  location: 'London',
-  previousBackground: 'Plumber',
-  className: 'WebEU 3',
-  favSubjects: ['JS', 'Node', 'Redux']
-}
 
 /*
   TASK 5
@@ -212,8 +200,12 @@ const studentAttr = {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+constructor(attributes) {
+  super(attributes);
+  this.previousBackground = attributes.previousBackground;
+  this.
+}
 }
 
 /*
